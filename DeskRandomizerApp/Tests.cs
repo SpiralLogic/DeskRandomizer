c1a0ef4fb2b7c09a92a55e3f731f236997e433c4
Sol Jennings <sol.jennings@myob.com>
2018-03-23 11:34:11 +1100

    First Tests

---
+++ DeskRandomizerApp/Tests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace DeskRandomizerApp
+{
+    public class Tests
+    {
+        [Fact]
+        public void CreatePersonListShouldCreateAnArrayWithGivenInput()
+        {
+            var deskRandomizer = new DeskRandomizerApp(3);
+
+            Assert.Equal(3, deskRandomizer.Arrangement.Length);
+        }
+    }
+}
