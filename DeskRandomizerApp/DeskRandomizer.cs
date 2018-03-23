f35c48892706cbfa62e9dab46c027211f9e3e541
Sol Jennings <sol.jennings@myob.com>
2018-03-23 11:45:58 +1100

    Changed arrangement to final arrangement

--- DeskRandomizerApp/DeskRandomizer.cs
+++ DeskRandomizerApp/DeskRandomizer.cs
@@ -1,14 +1,23 @@
-﻿using System.Runtime.InteropServices.ComTypes;
-
-namespace DeskRandomizerApp
+﻿namespace DeskRandomizerApp
 {
     public class DeskRandomizer
     {
+        public int[] FinalArrangement { get; private set; }
+        
         public DeskRandomizer(int numberOfPeople)
         {
-            Arrangement = new int[numberOfPeople];
+            InitializeFinalArrangement(numberOfPeople);
         }
 
-        public int[] Arrangement { get; }
+        private void InitializeFinalArrangement(int numberOfPeople)
+        {
+            FinalArrangement = new int[numberOfPeople];
+
+            for (var i = 0; i < numberOfPeople; i++)
+            {
+                FinalArrangement[i] = -1;
+            }
+        }
+
     }
 }
--- DeskRandomizerTests/Tests.cs
+++ DeskRandomizerTests/Tests.cs
@@ -10,7 +10,18 @@ namespace DeskRandomizerTests
         {
             var deskRandomizer = new DeskRandomizer(3);
 
-            Assert.Equal(3, deskRandomizer.Arrangement.Length);
+            Assert.Equal(3, deskRandomizer.FinalArrangement.Length);
+        }
+
+        [Fact]
+        public void ArrangementShouldBePopulated()
+        {
+            var deskRandomizer = new DeskRandomizer(3);
+
+            foreach (var element in deskRandomizer.FinalArrangement)
+            {
+                Assert.Equal(-1, element);
+            }
         }
     }
 }
