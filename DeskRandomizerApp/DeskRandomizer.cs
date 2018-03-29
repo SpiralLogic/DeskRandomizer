3f52b52f7ce3c07db832b195e13c1ecba99df27b
Sol Jennings <sol.jennings@myob.com>
2018-03-29 15:09:58 +1100

    All tests working, needs a console output

--- DeskRandomizerApp/DeskRandomizer.cs
+++ DeskRandomizerApp/DeskRandomizer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DeskRandomizerApp
@@ -6,17 +7,17 @@ namespace DeskRandomizerApp
     public class DeskRandomizer
     {
         private readonly int _numberOfPeople;
-        private int[] _personList;
-        public int[] FinalArrangement { get; private set; }
+        private int[] _people;
+        public int[] FinalArrangement { get; set; }
 
-        public int[] PersonList
+        public int[] People
         {
-            get => _personList;
+            get => _people;
             set
             {
                 if (value.Length != _numberOfPeople)
                     throw new ArgumentOutOfRangeException();
-                _personList = value;
+                _people = value;
             }
         }
 
@@ -31,11 +32,11 @@ namespace DeskRandomizerApp
 
         private void InitializePersonList()
         {
-            PersonList = new int[_numberOfPeople];
+            People = new int[_numberOfPeople];
 
             for (var i = 0; i < _numberOfPeople; i++)
             {
-                PersonList[i] = i + 1;
+                People[i] = i + 1;
             }
         }
 
@@ -49,34 +50,114 @@ namespace DeskRandomizerApp
             }
         }
 
-        public bool HasPreviousNeighbours(int i)
+        public bool HasPreviousNeighbours(int position, int person, int[] peopleList)
         {
-            return IsPersonWrappedPreviousNeighbour(i) || IsPersonToRightPreviousNeighbour(i) || IsPersonToLeftPreviousNeighbour(i);
+            return IsPersonWrappedPreviousNeighbour(position, person, peopleList) ||
+                   (IsPersonToRightPreviousNeighbour(position, person, peopleList) || IsPersonToLeftPreviousNeighbour(position, person, peopleList));
         }
 
-        private bool IsPersonWrappedPreviousNeighbour(int i)
+        private bool IsPersonWrappedPreviousNeighbour(int position, int person, int[] peopleList)
         {
-            if (i == 0)
+            if (position == 0)
             {
-                return PersonList.Last() + 1 == PersonList[0] || PersonList.Last() - 1 == PersonList[0];
+                return peopleList.Last() + 1 == peopleList[0] || peopleList.Last() - 1 == peopleList[0];
             }
 
-            if (i == _numberOfPeople - 1)
+            if (position == _numberOfPeople - 1)
             {
-                return PersonList.First() + 1 == PersonList[i] || PersonList.First() - 1 == PersonList[i];
+                return peopleList.First() + 1 == person || peopleList.First() - 1 == person;
             }
 
             return false;
         }
 
-        private bool IsPersonToRightPreviousNeighbour(int i)
+        private bool IsPersonToRightPreviousNeighbour(int position, int person, int[] peopleList)
         {
-            return PersonList[i + 1] + 1 == PersonList[i] || PersonList[i + 1] - 1 == PersonList[i];
+            if (position + 1 >= _numberOfPeople) return false;
+
+            return peopleList[position + 1] + 1 == person || peopleList[position + 1] - 1 == person;
         }
 
-        private bool IsPersonToLeftPreviousNeighbour(int i)
+        private bool IsPersonToLeftPreviousNeighbour(int position, int person, int[] peopleList)
         {
-            return PersonList[i - 1] + 1 == PersonList[i] || PersonList[i - 1] - 1 == PersonList[i];
+            if (position - 1 < 0) return false;
+
+            return peopleList[position - 1] + 1 == person || peopleList[position - 1] - 1 == person;
+        }
+
+        public int[] Allocate(List<int> peopleToPlace)
+        {
+            foreach (var person in peopleToPlace.Take(peopleToPlace.Count - 1))
+            {
+                PlaceNextPerson(person);
+            }
+
+            BeforePlacement = FinalArrangement.ToArray();
+            PlaceLastPerson(peopleToPlace.Last());
+            return FinalArrangement;
+            //      PlaceLastPerson(peopleToPlace.Last());
+        }
+
+        public int[] BeforePlacement { get; set; }
+
+        public void PlaceLastPerson(int lastPerson)
+        {
+            var freePosition = Array.IndexOf(FinalArrangement, -1);
+
+            if (!HasPreviousNeighbours(freePosition, lastPerson, FinalArrangement))
+            {
+                FinalArrangement[freePosition] = lastPerson;
+
+                return;
+            }
+
+            for (var i = 0; i < _numberOfPeople; i++)
+            {
+                if (i == freePosition) continue;
+
+                var testArrangement = FinalArrangement.ToArray();
+                testArrangement[freePosition] = FinalArrangement[i];
+                testArrangement[i] = lastPerson;
+                
+                if (!HasPreviousNeighbours(i, lastPerson, testArrangement) && !HasPreviousNeighbours(freePosition, FinalArrangement[i], testArrangement))
+                {
+                    FinalArrangement = testArrangement;
+                    return;
+                }
+            }
+        }
+
+        private void PlaceNextPerson(int person)
+        {
+            for (var i = 0; i < _numberOfPeople; i++)
+            {
+                if (FinalArrangement[i] == -1)
+                {
+                    if (!HasPreviousNeighbours(i, person, FinalArrangement))
+                    {
+                        FinalArrangement[i] = person;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static Random rng = new Random();
+
+        public void ShufflePeople()
+        {
+            var list = People.ToList();
+            var n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = rng.Next(n + 1);
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+
+            People = list.ToArray();
         }
     }
 }
--- DeskRandomizerTests/Tests.cs
+++ DeskRandomizerTests/Tests.cs
@@ -32,7 +32,7 @@ namespace DeskRandomizerTests
         {
             var deskRandomizer = new DeskRandomizer(3);
 
-            Assert.Equal(3, deskRandomizer.PersonList.Length);
+            Assert.Equal(3, deskRandomizer.People.Length);
         }
 
         [Fact]
@@ -40,7 +40,7 @@ namespace DeskRandomizerTests
         {
             var deskRandomizer = new DeskRandomizer(4);
 
-            foreach (var person in deskRandomizer.PersonList)
+            foreach (var person in deskRandomizer.People)
             {
                 Assert.True(person > 0);
             }
@@ -51,7 +51,7 @@ namespace DeskRandomizerTests
         {
             var deskRandomizer = new DeskRandomizer(4);
 
-            Assert.Equal(4, deskRandomizer.PersonList.Distinct().Count());
+            Assert.Equal(4, deskRandomizer.People.Distinct().Count());
         }
 
         [Fact]
@@ -59,50 +59,116 @@ namespace DeskRandomizerTests
         {
             var deskRandomizer = new DeskRandomizer(4);
             var testArray = new int[5];
-            Assert.Throws<ArgumentOutOfRangeException>(() => deskRandomizer.PersonList = testArray);
+            Assert.Throws<ArgumentOutOfRangeException>(() => deskRandomizer.People = testArray);
         }
-        
+
         [Fact]
         public void HasPreviousNeighboursReturnsTrueWithPreviousNeighboursOnRight()
         {
             var deskRandomizer = new DeskRandomizer(3);
 
-            deskRandomizer.PersonList = new List<int>{7,3,4}.ToArray();
-            var resultHigher = deskRandomizer.HasPreviousNeighbours(1);
+            var resultHigher = deskRandomizer.HasPreviousNeighbours(1, 3, new List<int> {7, 3, 4}.ToArray());
+            var resultLower = deskRandomizer.HasPreviousNeighbours(1, 3, new List<int> {7, 3, 4}.ToArray());
 
-            deskRandomizer.PersonList = new List<int>{7,3,2}.ToArray();
-            var resultLower = deskRandomizer.HasPreviousNeighbours(1);
-            
             Assert.True(resultHigher);
             Assert.True(resultLower);
         }
-        
+
         [Fact]
         public void HasPreviousNeighboursReturnsTrueWithPreviousNeighboursOnLeft()
         {
             var deskRandomizer = new DeskRandomizer(3);
 
-            deskRandomizer.PersonList = new List<int>{2,3,7}.ToArray();
-            var resultHigher = deskRandomizer.HasPreviousNeighbours(1);
+            var resultHigher = deskRandomizer.HasPreviousNeighbours(1, 3, new List<int> {2, 3, 7}.ToArray());
+            var resultLower = deskRandomizer.HasPreviousNeighbours(1, 3, new List<int> {2, 3, 7}.ToArray());
 
-            deskRandomizer.PersonList = new List<int>{4,3,7}.ToArray();
-            var resultLower = deskRandomizer.HasPreviousNeighbours(1);
-            
             Assert.True(resultHigher);
             Assert.True(resultLower);
         }
-        
+
         [Fact]
         public void HasPreviousNeighboursReturnsTrueWithPreviousNeighboursWrapped()
         {
             var deskRandomizer = new DeskRandomizer(3);
 
-            deskRandomizer.PersonList = new List<int>{2,7,1}.ToArray();
-            var resultLeft = deskRandomizer.HasPreviousNeighbours(0);
-            var resultRight = deskRandomizer.HasPreviousNeighbours(2);
-            
+            var resultLeft = deskRandomizer.HasPreviousNeighbours(0, 2, new List<int> {2, 7, 1}.ToArray());
+            var resultRight = deskRandomizer.HasPreviousNeighbours(2, 1, new List<int> {2, 7, 1}.ToArray());
+
             Assert.True(resultLeft);
             Assert.True(resultRight);
+        }
+
+        [Fact]
+        public void AllocatePlacesPeopleInFinalAllocation()
+        {
+            var deskRandomizer = new DeskRandomizer(6);
+
+            deskRandomizer.Allocate(deskRandomizer.People.ToList());
+
+            Assert.Equal(new List<int> {1, 4, 2, 5, 3, 6}.ToArray(), deskRandomizer.FinalArrangement);
+        }
+
+        [Fact]
+        public void AllocatePlacesPeopleInWithNoPreviousNeighbours()
+        {
+            var deskRandomizer = new DeskRandomizer(6);
+
+            deskRandomizer.Allocate(deskRandomizer.People.ToList());
+            var expected = new List<int> {1, 4, 2, 5, 3, 6}.ToArray();
+            Assert.Equal(expected, deskRandomizer.FinalArrangement);
+        }
+
+        [Fact]
+        public void AllocatePlacesPeopleInWithNoPreviousNeighboursWithShuffle()
+        {
+            var deskRandomizer = new DeskRandomizer(11);
+
+            deskRandomizer.ShufflePeople();
+            deskRandomizer.Allocate(deskRandomizer.People.ToList());
+
+            var passed = true;
+            for (var i = 0; i < 11; i++)
+            {
+                if (deskRandomizer.HasPreviousNeighbours(i, deskRandomizer.FinalArrangement[i], deskRandomizer.FinalArrangement))
+                {
+                    Assert.Equal(new List<int> {1, 4, 2, 5, 3, 6}.ToArray(), deskRandomizer.BeforePlacement);
+                    Assert.False(true);
+                }
+
+                ;
+            }
+        }
+
+        [Fact]
+        public void PlacesesLastPerson2()
+        {
+            var deskRandomizer = new DeskRandomizer(6);
+
+            deskRandomizer.Allocate(new List<int> {4, 3, 6, 5, 1, 2});
+
+            Assert.Equal(new List<int> {2, 6, 3, 5, 1, 4}.ToArray(), deskRandomizer.FinalArrangement);
+        }
+
+        [Fact]
+        public void PlacesesLastPerson()
+        {
+            var deskRandomizer = new DeskRandomizer(6);
+
+            deskRandomizer.Allocate(new List<int> {5, 1, 4, 6, 3, -1});
+
+            deskRandomizer.PlaceLastPerson(2);
+            Assert.Equal(new List<int> {5, 2, 4, 6, 3, 1}.ToArray(), deskRandomizer.FinalArrangement);
+        }
+
+        [Fact]
+        public void PlacesesLastPerson3()
+        {
+            var deskRandomizer = new DeskRandomizer(6);
+
+            deskRandomizer.Allocate(new List<int> {1, 5, 3, 6, 4, -1});
+
+            deskRandomizer.PlaceLastPerson(2);
+            Assert.Equal(new List<int> {1, 5, 3, 6, 2, 4}.ToArray(), deskRandomizer.FinalArrangement);
         }
     }
 }
